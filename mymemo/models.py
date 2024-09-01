f5cf4c367ddf4fefae904cf40971495e06bbc0cb
mocaron <mocaron00@ewhain.net>
2024-09-01 21:43:06 +0900

    9월1주차

--- mymemo/models.py
+++ mymemo/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Memo(models.Model) :
+    text = models.CharField(max_length = 200)
+    published_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
+    
+#정확히는 장고내에서 Memo라는 이름으로 생성되지는 않는다
--- mymemo/views.py
+++ mymemo/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import *
 
 # Create your views here.
 def index(request):
-    return HttpResponse("강서공고 안녕")
+    #return HttpResponse("강서공고 안녕")
+    return render(request, 'main.html')
+
+
+
+def create(request):
+    content = request.GET['memoContent']
+    return HttpResponse("메모작성 내용 = "+content)
