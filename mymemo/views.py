f1f42aae0e7ebd60fffe9017913b352fe00e5e55
mocaron <76911793+mocaron@users.noreply.github.com>
2024-09-22 22:14:38 +0900

    0923

--- mymemo/views.py
+++ mymemo/views.py
@@ -1,14 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import *
 
 # Create your views here.
 def index(request):
     #return HttpResponse("강서공고 안녕")
-    return render(request, 'main.html')
-
+    #return render(request, 'main.html')
+    lists = Memo.obejcts.all()
+    data = {'lists': lists}
+    return render(request 'main.html', data)
+    
 
 
 def create(request):
     content = request.POST['memoContent']
-    return HttpResponse("메모작성 내용 = "+content)
+    #return HttpResponse("메모작성 내용 = "+content)
+    usermemo = Memo(text = content)
+    usermemo.save()
+    return HttpResponseRedirect(reverse('index'))
+
