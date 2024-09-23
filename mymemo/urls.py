31c5f4d56a82555a705f044d6112fda628252210
mocaron <76911793+mocaron@users.noreply.github.com>
2024-09-24 01:26:25 +0900

    Update urls.py

--- mymemo/urls.py
+++ mymemo/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('',views.index, name='index'),
     path('create/', views.create),
+    path('edit/<str:idx>', views.editPage),
+    path('edit/update/', views.updateMemo),
 ]
